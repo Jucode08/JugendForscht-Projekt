855c79706a179be1c7657904d925287c1b87ac47
Jucode <julian0804h@gmail.com>
2025-06-06 10:59:25 +0200

    base

---
+++ learning.py
@@ -0,0 +1,7 @@
+import os
+import wave  # für Audio-Dateien
+import whisper  # Speech-to-Text
+from langdetect import detect  # Sprache erkennen
+from gtts import gTTS  # Text-to-Speech
+from pydub import AudioSegment  # Audio konvertieren/abspielen
+

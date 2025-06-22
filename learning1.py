b0619c75c2d40aa3576faca2289933ae21b505b4
Jucode08 <julian0804h@gmail.com>
2025-06-22 21:03:29 +0200

    testing

--- learning1.py
+++ learning1.py
@@ -1,8 +1,55 @@
 import os
+import numpy as np
 import wave  # für Audio-Dateien
 import whisper  # Speech-to-Text
+from googletrans import Translator
 from langdetect import detect  # Sprache erkennen
 from gtts import gTTS  # Text-to-Speech
-from pydub import AudioSegment  # Audio konvertieren/abspielen
+import soundfile as sf
+import ffmpeg
 
-model = whisper.load_model("tiny")  # Whisper-Modell laden
+#imports for testing
+import sounddevice as sd
+
+# for i in range(10):
+recordDuration = 4
+samplerate = 16000  # 16 kHz
+
+print("recording...")
+recording = sd.rec(int(recordDuration * samplerate), samplerate=samplerate, channels=1) #channels=1 => mono , channels=2 => stereo
+sd.wait()
+
+# Wandelt (N, 1) => (N,) um und castet auf float32
+recording = recording.flatten().astype(np.float32)
+
+# print("playing...")
+# sd.play(recording, samplerate=samplerate)
+# sd.wait()
+
+knownLanguages = ["de", "en"]
+model = whisper.load_model("tiny")
+
+# Whisper-Preprocessing
+audio = whisper.pad_or_trim(recording)
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+# logging
+_, probabilities = model.detect_language(mel)
+language = max(probabilities, key=probabilities.get)
+print("Sprache erkannt als:", language)
+print("Wahrscheinlichkeiten: ", "de: " , probabilities["de"], " en: " , probabilities["en"])
+
+# Transkription
+# options = whisper.DecodingOptions(fp16=False)  # wenn du keine GPU nutzt
+result = whisper.decode(model, mel)
+
+print(result.text)
+
+if language not in knownLanguages:
+    #translate
+    # ...
+    pass
+    
+tts = gTTS(text=result.text, lang=knownLanguages[0])
+tts.save("output.mp3")
+
